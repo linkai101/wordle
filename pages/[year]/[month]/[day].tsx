bef00d963601dbd545aea6f6c497111025872fbf
Linkai Wu <linkai@linkaiwu.com>
2022-04-06 14:28:46 +0000

    Fix internal error on future dates

--- pages/[year]/[month]/[day].tsx
+++ pages/[year]/[month]/[day].tsx
@@ -151,7 +151,9 @@ export default function ArchivePage({ wordleData }) {
         >
           <span className="flex items-center">
             {dates.length>0 &&
-              <span className="block truncate text-sm md:text-md">#{wordleData.id} - {dates[dates.length-1-wordleData.id].toLocaleDateString('en-US', { weekday: 'short', month: 'long', day: 'numeric' })}</span>
+              <span className="block truncate text-sm md:text-md">
+                #{wordleData.id} - {(new Date(`${wordleData.date.month}/${wordleData.date.day}/${wordleData.date.year}`)).toLocaleDateString('en-US', { weekday: 'short', month: 'long', day: 'numeric' })}
+              </span>
             }
           </span>
           <span className="ml-3 absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
@@ -168,6 +170,24 @@ export default function ArchivePage({ wordleData }) {
           tabIndex={-1}
           role="listbox"
         >
+          {dates.length-1-wordleData.id < 0 &&
+          <li
+            className="text-gray-900 cursor-default select-none relative py-2 pl-3 pr-2 text-sm md:text-md hover:bg-zinc-100"
+            role="option"
+            ref={currentEl}
+          >
+            <div className="flex items-center">
+              <span className="block truncate">#{wordleData.id} - {(new Date(`${wordleData.date.month}/${wordleData.date.day}/${wordleData.date.year}`)).toLocaleDateString('en-US', { weekday: 'short', month: 'long', day: 'numeric' })}</span>
+            </div>
+
+            <span className="text-indigo-600 absolute inset-y-0 right-0 flex items-center pr-2">
+              <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
+              </svg>
+            </span>
+          </li>
+          }
+
           {dates.map((d,i) =>
             <li
               className="text-gray-900 cursor-default select-none relative py-2 pl-3 pr-2 text-sm md:text-md hover:bg-zinc-100"
